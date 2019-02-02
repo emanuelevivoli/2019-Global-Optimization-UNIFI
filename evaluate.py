added Bayesian optimization; changes to evaluation functions; fixed loss calculation

evaluate.py
@@ -1,52 +1,56 @@
-import rbfopt
-import numpy as np
-
 import torch
 import torchvision
 import torchvision.transforms as transforms
 
-# TODO decidere quali iperparametri ottimizzare
+import rbfopt
+from bayes_opt import BayesianOptimization
+
+# TODO iperparametri ottimizati: learning_rate, weight_decay (regolarizzatore). Vedere se aggiungerne altri
 from net import Net
 
 
-def evaluate(hyperparameters):
-    lr = hyperparameters[0]   # learning rate
-    # reg = hyperparameters[1]  # regularizer
+def evaluate_RBF(hyperparameters):
+    return evaluate_BAY(hyperparameters[0], hyperparameters[1])
+
+def evaluate_BAY(learning_rate, weight_decay):
     max_epochs = 10
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    model = Net(max_epochs, lr).to(device)
+    model = Net(max_epochs, learning_rate, weight_decay).to(device)
 
-    # TODO allenamento modello
     training_losses = model.fit(trainloader)
 
-    # TODO valutazione dell'accuratezza sul test set
     accuracy, test_loss = model.eval_metrics(testloader)
 
     # TODO salvo su file di testo l'accuratezza, insieme alle informazioni collegate (ottimizzatore, val iperparametri, loss, numero tentativo ecc.)
-    print(hyp_opt + "(lr=" + str(lr) + "), max_epochs=" + str(max_epochs) + ": accuracy=" + str(accuracy) + ", test_loss="+str(test_loss))
+    print(hyp_opt + "(lr=" + str(learning_rate) + "), max_epochs=" + str(max_epochs) + ": train_loss=" + str(training_losses[-1]) +
+          ", test_loss=" + str(test_loss) + ", test_accuracy=" + str(accuracy))
 
     return test_loss
 
 
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+''' CIFAR10 Dataset reading'''
+transform = transforms.Compose([transforms.ToTensor(),
+                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                        download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=128,
-                                          shuffle=True, num_workers=6)
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=6)
 
-testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                       download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=128,
-                                         shuffle=False, num_workers=6)
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=128, shuffle=False, num_workers=6)
 
+
+''' Radial Basis Function hyperparameters optimization '''
 hyp_opt = "RBF"
-#bb = rbfopt.RbfoptUserBlackBox(3, np.array([0] * 3), np.array([10] * 3), np.array(['R', 'I', 'R']), evaluate)
-bb = rbfopt.RbfoptUserBlackBox(1, [0.00001], [0.001], ['R'], evaluate)
-
-settings = rbfopt.RbfoptSettings(max_evaluations=50)
+bb = rbfopt.RbfoptUserBlackBox(2, [0.00001, 0.0], [0.001, 0.001], ['R', 'R'], evaluate_RBF)
+settings = rbfopt.RbfoptSettings(max_evaluations=4)
 alg = rbfopt.RbfoptAlgorithm(settings, bb)
 val, x, itercount, evalcount, fast_evalcount = alg.optimize()
+
+
+''' Bayesian hyperparameters optimization '''
+hyp_opt = "BAY"
+pb = {"learning_rate": (0.00001, 0.001), "weight_decay": (0, 0.001)}
+bay_opt = BayesianOptimization(f=evaluate_BAY, pbounds=pb)
+bay_opt.maximize(init_points=2, n_iter=4)
+print(bay_opt.max)

net.py
@@ -10,7 +10,7 @@ import torch.optim as optim
 
 class Net(nn.Module):
 
-    def __init__(self, max_epochs, learning_rate, useGPU=True):
+    def __init__(self, max_epochs, learning_rate, weight_decay, useGPU=True):
         super(Net, self).__init__()
 
         # net layers
@@ -24,7 +24,7 @@ class Net(nn.Module):
         # loss function
         self.criterion = nn.CrossEntropyLoss()
         # loss optimizer
-        self.optimizer = optim.Adam(self.parameters(), lr=learning_rate)
+        self.optimizer = optim.Adam(self.parameters(), lr=learning_rate, weight_decay=weight_decay)
         # max training epochs
         self.max_epochs = max_epochs
 
@@ -48,6 +48,7 @@ class Net(nn.Module):
         for epoch in range(self.max_epochs):  # loop over the dataset multiple times
 
             running_loss = 0.0
+            loss_updates = 0
             for i, data in enumerate(trainloader, 0):
 
                 # get the inputs
@@ -67,14 +68,14 @@ class Net(nn.Module):
 
                 # loss update
                 running_loss += loss.item()
+                loss_updates += 1
 
                 # print statistics
                 if i % 2000 == 1999:  # print every 2000 mini-batches
-                    print('[%d, %5d] loss: %.3f' %
-                          (epoch + 1, i + 1, running_loss / 2000))
-                    running_loss = 0.0
+                    print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / loss_updates))
 
-            losses.append(running_loss)
+            print("epoch " + str(epoch+1) + "/" + str(self.max_epochs) + ": training_loss=" + str(running_loss/loss_updates), end="\r")
+            losses.append(running_loss/loss_updates)
 
         return losses
 
@@ -83,6 +84,7 @@ class Net(nn.Module):
         correct = 0
         total = 0
         loss = 0.0
+        num_batches = 0
         with torch.no_grad():
             for data in testloader:
 
@@ -97,11 +99,13 @@ class Net(nn.Module):
 
                 # loss update
                 loss += self.criterion(outputs, labels).item()
+                num_batches += 1
 
                 # update numbers of total and correct predictions
                 total += labels.size(0)
                 correct += (predicted == labels).sum().item()
 
         accuracy = 100 * correct / total
+        loss /= num_batches
         return round(accuracy, 1), loss
 